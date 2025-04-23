80e530753c413e3551f61cb77c56534e0e332d4f
rongzeng <zengrron@yahoo.com>
2025-04-23 19:20:46 -0400

    checkpoint commit; add plot pcn loss function

--- cifar_test.py
+++ cifar_test.py
@@ -13,16 +13,16 @@ from tqdm import tqdm
 from scipy.optimize import dual_annealing
 import numpy as np
 from utils import expand_weights_to_matrix, save_expanded_weights
-
+import matplotlib.pyplot as plt
 
 class PcConvBp_DS(nn.Module):
-    def __init__(self, inchan, outchan, kernel_size=3, stride=1, padding=1, cls=0, lr=1e-2, bias=False, 
-                 solver='SGD', num_iterations=5, train_weight=False, noise_level=None, weight_path=None, layer_idx=None):
+    def __init__(self, inchan, outchan, kernel_size=3, stride=1, padding=1, lr=1e-2, bias=False,
+                 solver='SGD', num_iterations=5, train_weight=False, noise_level=None, weight_path=None,
+                 layer_idx=None, plot_path=None):
         super().__init__()
         self.noise_level = noise_level
         self.solver = solver
         self.train_weight = train_weight
-        self.num_iterations = num_iterations
         self.padding = padding
         self.stride = stride
         self.kernel_size = kernel_size
@@ -32,21 +32,23 @@ class PcConvBp_DS(nn.Module):
         self.FBconv = nn.ConvTranspose2d(outchan, inchan, self.kernel_size, self.stride, self.padding, bias=bias)
         self.b0 = nn.ParameterList([nn.Parameter(torch.zeros(1, outchan, 1, 1))])
         self.relu = nn.ReLU(inplace=True)
-        self.cls = cls
+        self.num_iterations = num_iterations
         self.lr = lr
         self.bypass = nn.Conv2d(inchan, outchan, kernel_size=1, stride=1, bias=False)
         self.noise_ff_matrix = torch.randn_like(self.FFconv.weight) * (0.0 if noise_level is None else noise_level)
         self.noise_fb_matrix = torch.randn_like(self.FBconv.weight) * (0.0 if noise_level is None else noise_level)
 
+        self.plot_path = plot_path
         self.weight_path = weight_path
         self.layer_idx = layer_idx
-        self._load_expanded_weights()
+        self._load_expanded_weights(noise_level)
 
-    def forward(self, x, layer_idx):
+    def forward(self, x, layer_idx, w_type="fb_flip"):
+        # Todo: should we add noise to bypass and ff convolution?
         noise_ff = (self.noise_ff_matrix.to(device=self.FFconv.weight.device) + 1) * self.FFconv.weight
         y = self.relu(self.FFconv(x))
         # y = self.relu(torch.conv2d(x, noise_ff, padding=self.FFconv.padding))
-        y = self.find_optimal_r(x, y, layer_idx, solver=self.solver)
+        y = self.find_optimal_r(x, y, layer_idx, solver=self.solver, w_type=w_type)
         y = y + self.bypass(x)
         return y
 
@@ -65,8 +67,9 @@ class PcConvBp_DS(nn.Module):
             expanded_weights = expanded_weights.to(y.device)
             flattened_y = torch.flatten(y, start_dim=1).clone().detach().requires_grad_(True)
             flattened_y.retain_grad()
-            optimizer_y = torch.optim.SGD([flattened_y], lr=0.001)
-            optimizer_w = torch.optim.SGD([expanded_weights], lr=0.001) if self.train_weight else None
+            optimizer_y = torch.optim.SGD([flattened_y], lr=self.lr)
+            optimizer_w = torch.optim.SGD([expanded_weights], lr=self.lr) if self.train_weight else None
+            energy_list = []
             for _ in range(self.num_iterations):
                 optimizer_y.zero_grad()
                 energy = self.Energy_Function(flattened_x, expanded_weights, flattened_y)
@@ -74,8 +77,8 @@ class PcConvBp_DS(nn.Module):
                 # energy = (flattened_x - flattened_y @ expanded_weights.T).T.mm(flattened_x - flattened_y @ expanded_weights.T)
                 
                 energy.backward()
-                grad = flattened_y.grad
                 optimizer_y.step()
+                energy_list.append(energy.item())
                 
             if self.train_weight:
                 for _ in range(5):
@@ -84,6 +87,11 @@ class PcConvBp_DS(nn.Module):
                     energy.backward()
                     optimizer_w.step()
                     torch.save(expanded_weights, f'./expanded_weights_train/expanded_weights_{layer_idx}.pt')
+
+            if self.plot_path is not None:
+                plot_save_path = os.path.join(self.plot_path,
+                                         'pcn_loss_layer_{}_{}_lr_{}.pdf'.format(layer_idx, w_type, self.lr))
+                self.plot_and_save(energy_list, plot_save_path, plot_title=None)
             
         elif solver == 'SA':
             flattened_x_np = flattened_x.cpu().numpy()
@@ -102,7 +110,7 @@ class PcConvBp_DS(nn.Module):
             flattened_y = torch.tensor(result.x, dtype=torch.float32)
             
         elif solver == 'LD':
-            def LD(r0, r1, lr=1e-4, sd0=0.5, sd1=0.1):
+            def LD(r0, r1, lr=self.lr, sd0=0.5, sd1=0.1):
                 # Q is  W.T @ W
                 # c is  -2 * r0 @ W
                 with torch.no_grad():
@@ -133,7 +141,7 @@ class PcConvBp_DS(nn.Module):
         
         return optimal_y.detach()
 
-    def _load_expanded_weights(self):
+    def _load_expanded_weights(self, noise_level):
         fb_weight = torch.load(os.path.join(self.weight_path, 'expanded_weights_layer_fb_{}.pt'.format(self.layer_idx + 1)))
         fb_weight_flip = torch.load(
             os.path.join(self.weight_path, 'expanded_weights_layer_fb_{}_flip.pt'.format(self.layer_idx + 1)))
@@ -142,27 +150,51 @@ class PcConvBp_DS(nn.Module):
         bp_weight = torch.load(
             os.path.join(self.weight_path, 'expanded_weights_layer_bp_{}.pt'.format(self.layer_idx + 1)))
         self.expanded_weights = {"fb": fb_weight, "fb_flip": fb_weight_flip, "ff": ff_weight, "bp": bp_weight}
+        if noise_level is not None:
+            for _w_type, _weight in self.expanded_weights.items():
+                noise_ = torch.randn_like(_weight) * noise_level
+                self.expanded_weights[_w_type] = _weight * (1 + noise_)
 
     @staticmethod
     def Energy_Function(x, W, y):
         energy = torch.sqrt(x @ x.T -2* x @ W @ y.T + (y @ W.T) @ (W @ y.T))
         return energy
 
+    def plot_and_save(self, energy_list, plot_save_path, plot_title=None):
+        plt.rcParams['font.family'] = 'Times New Roman'
+
+        # Create the plot
+        fig, ax = plt.subplots()
+        ax.plot(range(len(energy_list)), [_eng ** 2 for _eng in energy_list])
+
+        # Labeling
+        ax.set_xlabel('Iteration', fontname='Times New Roman')
+        ax.set_ylabel('PCN loss', fontname='Times New Roman')
+        if plot_title:
+            ax.set_title(plot_title, fontname='Times New Roman')
+
+        # Improve layout and save
+        plt.tight_layout()
+        fig.savefig(plot_save_path, format='pdf')
+        plt.close(fig)
+
     
 ''' Architecture PredNetBpD '''
 from prednet import PcConvBp
 class PredNetBpD(nn.Module):
-    def __init__(self, num_classes=10, cls=0, lr=1e-2,
+    def __init__(self, num_classes=10, cls=0, lr=1e-4,
                  solver=None, layer_number=None, num_iterations=None, train_weight=False,
-                 noise_level=None, weight_path=None):
+                 noise_level=None, weight_path=None, plot_path=None, pcn_weight_type="fb_flip"):
         super().__init__()
         self.ics = [ 3, 32, 64,  64, 128] # input chanels
         self.ocs = [32, 64, 64, 128, 128] # output chanels
         self.maxpool = [False, True, False, True, False] # downsample flag
         self.cls = cls # num of time steps
         self.nlays = len(self.ics)
+        self.pcn_weight_type = pcn_weight_type
 
         # construct PC layers
+        self.solver = solver
         if solver is None:
             print('No solver in used, still using convolution in recurrent layer')
             assert layer_number is None, 'layer_number must be None if solver is None'
@@ -175,9 +207,10 @@ class PredNetBpD(nn.Module):
             for i in range(self.nlays):
                 # if i <= (layer_number-1):
                 if i in layer_number:
-                    self.PcConvs.append(PcConvBp_DS(self.ics[i], self.ocs[i], cls=self.cls, lr=lr,
+                    self.PcConvs.append(PcConvBp_DS(self.ics[i], self.ocs[i], lr=lr,
                                                     solver=solver, num_iterations=num_iterations, train_weight=train_weight,
-                                                    noise_level=noise_level, weight_path=weight_path, layer_idx=i))
+                                                    noise_level=noise_level, weight_path=weight_path, layer_idx=i,
+                                                    plot_path=plot_path))
                 else:
                     self.PcConvs.append(PcConvBp(self.ics[i], self.ocs[i], cls=self.cls, lr=1e-2))
         else:
@@ -194,7 +227,10 @@ class PredNetBpD(nn.Module):
     def forward(self, x):
         for i in range(self.nlays):
             x = self.BNs[i](x)
-            x = self.PcConvs[i](x, i)  # ReLU + Conv
+            if self.solver in ['SGD', 'SA', 'LD']:
+                x = self.PcConvs[i](x, i, self.pcn_weight_type)  # ReLU + Conv
+            else:
+                x = self.PcConvs[i](x, i)
             if self.maxpool[i]:
                 x = self.maxpool2d(x)
 
@@ -225,7 +261,7 @@ if __name__ == '__main__':
     # Create a DataLoader for the subset
     testloader = torch.utils.data.DataLoader(test_subset, batch_size=batchsize, shuffle=False, num_workers=2)
 
-    # Save expanded weights
+    # Load model
     model_path = 'checkpoint/PredNetBpD_5_30CLS_FalseNes_0.001WD_FalseTIED_4REP_best_ckpt.t7'
     # checkpoint_weight = torch.load('checkpoint/PredNetBpD_5_0CLS_FalseNes_0.001WD_FalseTIED_1REP_last_ckpt_no_recurr.t7', map_location=device)
     checkpoint_weight = torch.load(model_path, map_location=device)
@@ -235,16 +271,25 @@ if __name__ == '__main__':
     prednet = prednet.to(device)
     prednet = nn.DataParallel(prednet)
     prednet.load_state_dict(checkpoint_weight['net'])
-    sample_imgs, _ = next(iter(testloader))
-    save_expanded_weights(prednet, sample_imgs.to(device), "expanded_weights/" + model_path.split('/')[-1].split('.t7')[0])
+
+    # Save expanded weights (optional)
+    save_weight = True
+    weight_path = os.path.join("expanded_weights", model_path.split('/')[-1].split('.t7')[0])
+    plot_loss_path = os.path.join("loss_plot", model_path.split('/')[-1].split('.t7')[0])
+    if save_weight:
+        sample_imgs, _ = next(iter(testloader))
+        save_expanded_weights(prednet, sample_imgs.to(device), weight_path)
+        exit(0)
 
     # noise experiments
     for noise_level in [0, 0.05, 0.1, 0.15, .20, .25, .30, .35, .40]:
         trials = 3 if noise_level > 0 else 1
         for t in range(trials):
-            prednet = PredNetBpD(num_classes=10, cls=30, lr=1e-2,
-                                noise_level=noise_level,
-                                solver='LD', layer_number=[0, 1, 2, 3, 4], num_iterations=3000, train_weight=False)
+            prednet = PredNetBpD(num_classes=10, cls=30, lr=1e-4,
+                                 noise_level=noise_level, solver='SGD',
+                                 layer_number=[0, 1, 2, 3, 4], num_iterations=3000,
+                                 train_weight=False,
+                                 weight_path=weight_path, plot_path=plot_loss_path)
             prednet = prednet.to(device)
             prednet = nn.DataParallel(prednet)
             prednet.load_state_dict(checkpoint_weight['net'])
