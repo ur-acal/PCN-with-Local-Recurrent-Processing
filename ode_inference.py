c95917370d6713210dba039eb88ed3074002a4ab
rongzeng <zengrron@yahoo.com>
2026-01-26 16:51:44 -0500

    Change eval mismatch levels

--- ode_inference.py
+++ ode_inference.py
@@ -274,7 +274,7 @@ def run_ode_inference():
         offset_eps_list = [0.2, 0.35, 0.5, 0.65, 0.8] if args.sde_noise_type == "mul" else [None, 0.05, 0.1, 0.15, 0.2]
     else:
         offset_eps_list = [None]
-    noise_level_list_ = [0, 0.1, 0.15, 0.2, 0.3, 0.4]
+    noise_level_list_ = [0, 0.15, 0.2]
     noisy_trials = 20
     if args.test_expanded:
         # Too time-consuming, only run one mismatch level.
