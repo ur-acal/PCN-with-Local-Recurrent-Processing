da733b6396ade1a64609051083259d78fbb69e79
rongzeng <zengrron@yahoo.com>
2026-04-13 21:17:18 -0400

    modify the search config script to search for 2 stage conv model config

--- launch_scripts/slurm_search_config.sh
+++ launch_scripts/slurm_search_config.sh
@@ -26,18 +26,26 @@ IMG_TYPES=( "scanGFI" )
 CIRC_CONFS=( "" )
 
 # CHAN_0 options (order matters)
-CHAN_0_LIST=( 22 24 26 28 )
+#CHAN_0_LIST=( 22 24 26 28 ) # For the default three pattern
+CHAN_0_LIST=( 40 42 44 46 48 ) # For the time-interleaving
 
 # NUM_LAYERS dict: key=CHAN_0, value="layers..."
 declare -A NUM_LAYERS_BY_CHAN0
-NUM_LAYERS_BY_CHAN0[18]="20 22"
-NUM_LAYERS_BY_CHAN0[20]="18 20"
-NUM_LAYERS_BY_CHAN0[22]="22 24"
-NUM_LAYERS_BY_CHAN0[24]="16 18"
-NUM_LAYERS_BY_CHAN0[26]="16 18"
-NUM_LAYERS_BY_CHAN0[28]="14 16"
-NUM_LAYERS_BY_CHAN0[30]="12 14"
-NUM_LAYERS_BY_CHAN0[32]="10 12"
+# For the default three pattern
+#NUM_LAYERS_BY_CHAN0[18]="20 22"
+#NUM_LAYERS_BY_CHAN0[20]="18 20"
+#NUM_LAYERS_BY_CHAN0[22]="22 24"
+#NUM_LAYERS_BY_CHAN0[24]="16 18"
+#NUM_LAYERS_BY_CHAN0[26]="16 18"
+#NUM_LAYERS_BY_CHAN0[28]="14 16"
+#NUM_LAYERS_BY_CHAN0[30]="12 14"
+#NUM_LAYERS_BY_CHAN0[32]="10 12"
+# For the time-interleaving
+NUM_LAYERS_BY_CHAN0[40]="7"
+NUM_LAYERS_BY_CHAN0[42]="6"
+NUM_LAYERS_BY_CHAN0[44]="6"
+NUM_LAYERS_BY_CHAN0[46]="5"
+NUM_LAYERS_BY_CHAN0[48]="5"
 
 TRAIN_MODE="kd_crd_ft" # "kd_crd_ft", "train_ft", "mix_all"
 REPO_ROOT="/scratch/rzeng7/repos/PCN-with-Local-Recurrent-Processing"
@@ -56,12 +64,13 @@ MERGE_SCRIPT="${REPO_ROOT}/shell_utils/merge_csvs.py"
 
 SHOW_COMB_ONLY="${SHOW_COMB_ONLY:-0}"   # 1 => print comb_tag only and exit
 COMB_MODE="${COMB_MODE:-balanced_A}"      # choose in (n_params | balanced_A)
+SEARCH_ARCH="${SEARCH_ARCH:-two_stage_fixed}"     # choose in (default | two_stage_fixed)
 
 SUMMARY_CSV_SCRIPT="${REPO_ROOT}/shell_utils/summary_csvs_as_dict.py"
 ####################################################
 # Change the saved pickle file name here
 ####################################################
-SUMMARY_PKL_OUT="${MERGE_OUT_DIR}/summary_dict_0317_kdcrd_then_ft_AvgPool.pkl"
+SUMMARY_PKL_OUT="${MERGE_OUT_DIR}/summary_dict_0413_kdcrd_then_ft_AvgPool_TwoConvStage.pkl"
 ####################################################
 # Change EXP in submit_chunk
 ####################################################
@@ -76,11 +85,22 @@ generate_combs() {
   local num_layers="$2"
   local k="$3"
 
-  case "$COMB_MODE" in
-    n_params)    generate_top_combs   "$chan0" "$num_layers" "$k" ;;
-    balanced_A)  generate_ruleA_combs "$chan0" "$num_layers" "$k" ;;
+  case "$SEARCH_ARCH" in
+    default)
+      case "$COMB_MODE" in
+        n_params)    generate_top_combs   "$chan0" "$num_layers" "$k" ;;
+        balanced_A)  generate_ruleA_combs "$chan0" "$num_layers" "$k" ;;
+        *)
+          echo "ERROR: Unknown COMB_MODE='$COMB_MODE' (expected: n_params | balanced_A)" >&2
+          return 2
+          ;;
+      esac
+      ;;
+    two_stage_fixed)
+      generate_two_stage_fixed_combs "$chan0" "$num_layers" "$k"
+      ;;
     *)
-      echo "ERROR: Unknown COMB_MODE='$COMB_MODE' (expected: n_params | balanced_A)" >&2
+      echo "ERROR: Unknown SEARCH_ARCH='$SEARCH_ARCH' (expected: default | two_stage_fixed)" >&2
       return 2
       ;;
   esac
@@ -240,6 +260,52 @@ generate_ruleA_combs() {
     | sort -n -k1,1 -k2,2 -k3,3r \
     | head -n "${k}" \
     | awk -F'\t' 'BEGIN{OFS="\t"} {print $3,$4,$5,$6,$7,$8,$9}'
+}
+
+generate_two_stage_fixed_combs() {
+  local chan0="$1"
+  local num_layers="$2"
+  local k="$3"
+
+  local chan1=$((chan0 * 2))
+  local K2=9  # 3x3
+
+  local -a INP OUT POOL
+  local i params
+  local comb_tag
+
+  INP=(); OUT=(); POOL=()
+
+  # 3 -> chan0
+  INP+=(3); OUT+=("$chan0"); POOL+=(0)
+
+  # chan0 -> chan0 * num_layers
+  for ((i=1; i<=num_layers; i++)); do
+    INP+=("$chan0"); OUT+=("$chan0"); POOL+=(0)
+  done
+
+  # chan0 -> chan1, only pooling after this layer
+  INP+=("$chan0"); OUT+=("$chan1"); POOL+=(1)
+
+  # chan1 -> chan1 * num_layers
+  for ((i=1; i<=num_layers; i++)); do
+    INP+=("$chan1"); OUT+=("$chan1"); POOL+=(0)
+  done
+
+  params=$(( K2 * (
+    3*chan0 +
+    num_layers*chan0*chan0 +
+    chan0*chan1 +
+    num_layers*chan1*chan1
+  ) ))
+
+  comb_tag="TwoStage_N${num_layers}_C${chan0}"
+
+  # keep same output format as existing generate_combs:
+  # params \t comb_tag \t chan0 \t num_layers \t inp_str \t out_str \t pool_str
+  printf '%s\t%s\t%s\t%s\t%s\t%s\t%s\n' \
+    "${params}" "${comb_tag}" "${chan0}" "${num_layers}" \
+    "${INP[*]}" "${OUT[*]}" "${POOL[*]}"
 }
 
 # Submit ONE sbatch job for the current chunk.
